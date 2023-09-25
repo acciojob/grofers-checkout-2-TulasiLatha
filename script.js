96c8676b4e0b8ce8d63237d34e991e06fa3a2175
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-09-25 11:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+
+function calculateTotalPrice() {
+    let total = 0;
+    priceElements.forEach(priceElement => {
+        const price = parseFloat(priceElement.textContent);
+        if (!isNaN(price)) {
+            total += price;
+        }
+    });
+    return total.toFixed(2); // Ensure two decimal places
+}
+grandTotalElement.textContent = calculateTotalPrice();
+
+// Update the total price when prices change
+priceElements.forEach(priceElement => {
+    priceElement.addEventListener('input', () => {
+        grandTotalElement.textContent = calculateTotalPrice();
+    });
+});
